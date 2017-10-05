473fb9c8f681aded3511a7553595d2085a3b1775
DanieleSpera <scootyit@yahoo.it>
2017-10-05 23:37:27 +0200

    Scene Assets
    Scene Assets

---
+++ Assets/Scripts/CruscarpMoving.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CruscarpMoving : MonoBehaviour {
+
+    System.Random getrandom = new System.Random();
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("MoveCruscarp", 0, 6);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    private void MoveCruscarp()
+    {
+        GameObject cruscarp = this.gameObject;
+        Vector3 newPosition = new Vector3(getrandom.Next(-15, 15), 0, getrandom.Next(-15, 15));
+        iTween.MoveTo(cruscarp, newPosition, 3.0f);
+    }
+}
---
+++ Assets/Scripts/GhostMoving.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostMoving : MonoBehaviour
+{
+
+    System.Random getGhostrandom = new System.Random();
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("MoveCruscarp", 7, 6);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    private void MoveCruscarp()
+    {
+        GameObject cruscarp = this.gameObject;
+        Vector3 newPosition = new Vector3(getGhostrandom.Next(-15, 15), 0, getGhostrandom.Next(-15, 15));
+        iTween.MoveTo(cruscarp, newPosition, 3.0f);
+    }
+}
